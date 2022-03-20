1aecfe759f8d90fcd6c7dd8f41fc041a3b8f6b21
tonny-bright.sogli <tonny-bright.sogli@dreamoval.com>
2022-03-20 17:27:37 +0000

    changed the compression on whitenoise

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -148,6 +148,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CART_SESSION_ID = 'cart'
